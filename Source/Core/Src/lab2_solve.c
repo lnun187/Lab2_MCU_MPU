adab5098b220f758cc4f12301d478ad8949eec3c
lnun187 <167327421+lnun187@users.noreply.github.com>
2024-09-24 11:21:23 +0700

    sche Lab2 + Led blinky

---
+++ Source/Core/Src/lab2_solve.c
@@ -0,0 +1,28 @@
+/*
+ * lab2_solve.c
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: Admin
+ */
+#include "lab2_solve.h"
+
+void ex1_init();
+void ex1_run();
+void ex2_init();
+void ex2_run();
+void ex3_init();
+void ex3_run();
+void ex4_init();
+void ex4_run();
+void ex5_init();
+void ex5_run();
+void ex6_init();
+void ex6_run();
+void ex7_init();
+void ex7_run();
+void ex8_init();
+void ex8_run();
+void ex9_init();
+void ex9_run();
+void ex10_init();
+void ex10_run();
